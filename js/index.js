944cbdefd4699d20bf3cb7046be7e6b9c856d22e
marcosamado <led_9dejulio@hotmail.com>
2023-03-21 19:43:41 -0300

    agregue menu desplegable y botones de login en responsive

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 // *************** HEADER ***************//
 
-// ***Boton Hamburguesa y menu desplegable***
+// ***Boton Hamburguesa y menu desplegable responsive***
 const barMenu = document.querySelector(".bars_menu");
 const bar1 = document.querySelector(".line1_bars-menu");
 const bar2 = document.querySelector(".line2_bars-menu");
 const bar3 = document.querySelector(".line3_bars-menu");
 
 const menuDesplegable = document.getElementById("menu-desplegable");
-
+const backMenu = document.querySelector(".back_menu");
 
 barMenu.addEventListener("click", () => {
     bar1.classList.toggle("activeline1_bars-menu");
     bar2.classList.toggle("activeline2_bars-menu");
     bar3.classList.toggle("activeline3_bars-menu");
     menuDesplegable.classList.toggle("nav-desplegable");
+    mostrarMenu();
 })
 
-//***** Menu Desplegable ***
 
