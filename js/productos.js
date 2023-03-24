833c4e40d1768e3736e1128432611ba620b3ab4b
SilvaRocio <silvaagostina20@gmail.com>
2023-03-24 20:52:20 -0300

    iniciando trabajo en productos

--- js/productos.js
+++ js/productos.js
@@ -15,3 +15,32 @@ barMenu.addEventListener("click", () => {
     bar3.classList.toggle("activeline3_bars-menu");
     menuDesplegable.classList.toggle("nav-desplegable");
 })
+/********************** MAIN*************************************** */
+//*********** Recuperar productos de la api **********************/
+const card=document.getElementById("cards");
+const templateCard= document.getElementById("template-card").content;
+const fragment =document.createDocumentFragment();
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+})
+const fetchData= async()=>{
+    try {
+        const respuesta= await fetch("apiProductos.json");
+        const data= await respuesta.json();
+        cargarTarjetas(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function cargarTarjetas(data){
+    data.forEach((juego) => {
+        templateCard.querySelector(".titulo").innerText= juego.title;
+        templateCard.querySelector(".precio").innerText=juego.precio;
+        templateCard.querySelector("img").setAttribute("src", juego.Url);
+        templateCard.querySelector(".btn-dark").dataset.id=juego.id;
+        const clone= templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    });
+        card.appendChild(fragment);
+}
