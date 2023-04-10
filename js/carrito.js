9be3b822b3e1fbdde6247a6ce681b97bba83cf2e
SilvaRocio <silvaagostina20@gmail.com>
2023-04-10 10:36:42 -0300

    trabajando en js de carrito

--- js/carrito.js
+++ js/carrito.js
@@ -15,3 +15,50 @@ barMenu.addEventListener("click", () => {
     bar3.classList.toggle("activeline3_bars-menu");
     menuDesplegable.classList.toggle("nav-desplegable");
 });
+
+/******************MAIN ************************/
+
+// ****** Capturamos elementos *********//
+const cajaProductos=document.getElementById("items-body");
+const cajaFooter=document.getElementById("items-footer");
+const templateProductos=document.getElementById("template-productos").content;
+const templateFooter=document.getElementById("template-footer").content;
+
+
+const fragment = document.createDocumentFragment();
+
+document.addEventListener("DOMContentLoaded", () => {
+    cargarTemplateProductos();
+})
+//**********Capturamos el objeto con los productos del HTML productos****/
+
+let productoCarrito=localStorage.getItem("carrito");
+productoCarrito=JSON.parse(productoCarrito);
+// console.log(productoCarrito);
+
+
+function cargarTemplateProductos(){
+    Object.values(productoCarrito).forEach(producto=>{
+        console.log(producto.id);
+        templateProductos.getElementById("Producto").textContent=producto.titulo;
+        templateProductos.getElementById("Cantidad").innerHTML=`<td id="Cantidad">${producto.cantidad}
+                                                                    <button id="${producto.id}" class="agregar">+</button>
+                                                                    <button id="${producto.id}" class="quitar botonQuitar">-</button>
+                                                                    </td>`
+        // templateProductos.querySelector(".agregar").dataset.id=producto.id;
+        // templateProductos.querySelector(".quitar").textContent;
+        templateProductos.getElementById("Total").textContent=(producto.precio * producto.cantidad);
+        const clone=templateProductos.cloneNode(true);
+        fragment.appendChild(clone);
+    });
+    // console.log(fragment)
+    cajaProductos.appendChild(fragment);
+};
+
+
+
+
+
+
+
+
